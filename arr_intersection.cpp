68365448c78103901ce332134def6e6f67acbe6f
LaffRM <31437912+LaffRM@users.noreply.github.com>
2019-01-29 15:05:41 +0300

    Update arr_intersection.cpp

--- arr_intersection.cpp
+++ arr_intersection.cpp
@@ -2,8 +2,6 @@
 #include <unordered_map>
 #include <vector>
 
-//using namespace std;
-
 std::vector<int> arr_intersection(const std::vector<int> arr_A, const std::vector<int> arr_B)
 {
         std::unordered_map<int,int> hash_map;
